daa4e9179bfbf3a7d797308bd96998dcb9461554
Prasad48432 <prasadreddymar6@gmail.com>
2025-03-24 22:16:26 +0530

    fixed invalid states in exam page

--- src/app/exams/[examId]/page.tsx
+++ src/app/exams/[examId]/page.tsx
@@ -9,49 +9,114 @@ import db from "@/lib/db/db";
 import { isJsonValueEmpty } from "./utils/isJsonEmpty";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import { BookCheck, FileWarning, IdCard, TicketX } from "lucide-react";
 
-
-
-type Params = Promise<{ examId: string }>
-type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>
+type Params = Promise<{ examId: string }>;
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
 export default async function Page(props: {
-  params: Params
-  searchParams: SearchParams
+  params: Params;
+  searchParams: SearchParams;
 }) {
   const session = await auth();
   if (!session) redirect("/sign-in");
 
-  const params = await props.params
-  const searchParams = await props.searchParams
+  const params = await props.params;
+  const searchParams = await props.searchParams;
 
-
-  const  examId  = params.examId;
+  const examId = params.examId;
   const attemptId = searchParams.attemptId;
 
-
   if (!attemptId) {
-    return <div>Invalid Attempt</div>;
+    return (
+      <div className="h-screen w-full">
+        <div className="flex flex-col items-center justify-center gap-4 max-w-xl h-full mx-auto text-foreground/70">
+          <IdCard size={80} />
+          <p className="text-xl font-semibold">Attempt ID Not Found</p>
+          <p className="text-center">
+            Please provide Attempt ID along with provided Exam Id and try again
+            find info at{" "}
+            <a
+              href="/exams"
+              className="underline text-primary/80 underline-offset-1"
+            >
+              /exams
+            </a>
+          </p>
+        </div>
+      </div>
+    );
   }
 
   const exam = await getExamDetails(examId);
 
   if (!exam || !exam.data) {
-    return <div>Invalid Exam</div>;
+    return (
+      <div className="h-screen w-full">
+        <div className="flex flex-col items-center justify-center gap-4 max-w-xl h-full mx-auto text-foreground/70">
+          <FileWarning size={80} />
+          <p className="text-xl font-semibold">Exam Not Found</p>
+          <p className="text-center">
+            We couldn't find an exam with the provided ID. Please double-check
+            and try again. find exams at{" "}
+            <a
+              href="/exams"
+              className="underline text-primary/80 underline-offset-1"
+            >
+              /exams
+            </a>
+          </p>
+        </div>
+      </div>
+    );
   }
 
   const attempt = await getAttemptDetails(attemptId as string);
 
   if (!attempt || !attempt.data) {
-    return <div>Attempt not found</div>;
+    return (
+      <div className="h-screen w-full">
+        <div className="flex flex-col items-center justify-center gap-4 max-w-xl h-full mx-auto text-foreground/70">
+          <TicketX size={80} />
+          <p className="text-xl font-semibold">Invalid Attempt Id</p>
+          <p className="text-center">
+            There is something wrong with the attempt id provided. please
+            doucle-check the id and try again. find exams at{" "}
+            <a
+              href="/exams"
+              className="underline text-primary/80 underline-offset-1"
+            >
+              /exams
+            </a>
+          </p>
+        </div>
+      </div>
+    );
   }
 
-  if(attempt.data.userId !== session.user?.id){
+  if (attempt.data.userId !== session.user?.id) {
     return <div>Invalid user</div>;
   }
 
-  if(attempt.data.status === 'completed'){
-    return <div>Exam already completed</div>
+  if (attempt.data.status === "completed") {
+    return (
+      <div className="h-screen w-full">
+        <div className="flex flex-col items-center justify-center gap-4 max-w-xl h-full mx-auto text-foreground/70">
+          <BookCheck className="text-chart-success/70" size={80} />
+          <p className="text-xl font-semibold">Exam already completed</p>
+          <p className="text-center">
+            You have already completed this exam. Review your results or contact
+            support if needed. find result at{" "}
+            <a
+              href={`/dashboard/results/${attemptId}`}
+              className="underline text-primary/80 underline-offset-1"
+            >
+              /result
+            </a>
+          </p>
+        </div>
+      </div>
+    );
   }
 
   let questions: Question[] = [];
@@ -77,9 +142,9 @@ export default async function Page(props: {
   const bookmarkedQuestions = await db.bookmarkedQuestion.findMany({
     where: {
       userId: attempt.data.userId,
-      examId: attempt.data.examId
-    }
-  })
+      examId: attempt.data.examId,
+    },
+  });
 
   return (
     <div>
