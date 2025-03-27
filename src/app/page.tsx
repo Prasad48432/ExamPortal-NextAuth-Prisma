850b61b34a2ee3c7c9fceec602a9d5f6ed26be57
Prasad48432 <prasadreddymar6@gmail.com>
2025-03-27 14:22:54 +0530

    feature showcase added in home (placeholders)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import HeroPage from "@/components/hero";
+import FeatureShowcase from "@/components/featureshowacase";
 
 const Page = async () => {
-
   return (
+    <>
       <HeroPage />
+      <FeatureShowcase />
+    </>
   );
 };
 
